52bf7fa6f2bc86e5fa1e001e25329ceaf6192f09
skb399 <stefanboakye@gmail.com>
2026-02-16 20:58:43 +0000

    set up structure of game functions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,28 @@
-console.log("connected!")
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+function incrementScore() {
+
+}   
+
+function incrementWrongAnswer() {
+
+}   
+function displayAdditionQuestion(operand1, operand2) {
+
+} 
+function displaySubtractQuestion(operand1, operand2) {
+
+}
+function displayMultiplyQuestion(operand1, operand2) {      
+
+}
