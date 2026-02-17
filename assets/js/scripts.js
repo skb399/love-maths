d0bacdeaf323fd9537e4347a242bb6c3d71dbe3c
skb399 <stefanboakye@gmail.com>
2026-02-17 20:06:33 +0000

    add code to calculate 2 random numbers

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,20 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
 
 function runGame() {
 
@@ -10,19 +27,23 @@ function checkAnswer() {
 function calculateCorrectAnswer() {
 
 }
+
 function incrementScore() {
 
-}   
+}
 
 function incrementWrongAnswer() {
 
-}   
-function displayAdditionQuestion(operand1, operand2) {
+}
 
-} 
-function displaySubtractQuestion(operand1, operand2) {
+function displayAdditionQuestion() {
 
 }
-function displayMultiplyQuestion(operand1, operand2) {      
 
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
 }
