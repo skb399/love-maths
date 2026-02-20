16b8f340f8791c494c7827c4620fc91c8279c029
skb399 <stefanboakye@gmail.com>
2026-02-20 17:31:29 +0000

    add favicons and final tidy ups for user

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,11 +17,24 @@ document.addEventListener("DOMContentLoaded", function () {
     runGame("addition")
 });
 
+document.getElementById("answer-box").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        checkAnswer()
+    }
+})
+
+
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+    // Clear the answer box for the next question
+    document.getElementById("answer-box").value = "";
+
+    // The focus function ensures that the cursor is in the answer box when the game is run
+    document.getElementById("answer-box").focus();
+
     // Generate two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
